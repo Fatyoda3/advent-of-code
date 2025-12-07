b991ffdfbbf7c55e4e40cb2a99ff47182ba31e84
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-07 21:41:15 +0530

    `another one `

--- 2019/day_3/3.js
+++ 2019/day_3/3.js
@@ -6,16 +6,11 @@ const deltas = {
   D: { dx: 0, dy: - 1 }
 };
 
-const operations = (path) => path.map(([heading, ...rest]) => ({
+const operations = (path) => path.map(([heading, ...steps]) => ({
   heading,
-  val: parseInt(rest.join(''))
+  val: parseInt(steps.join(''))
 }));
 
-const steps1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'.split(',');
-const steps2 = 'U62,R66,U55,R34,D71,R55,D58,R83'.split(',');
-
-const wire1 = { x: 0, y: 0 };
-const wire2 = { x: 0, y: 0 };
 
 function traceSteps(path, wire) {
 
@@ -34,7 +29,6 @@ function traceSteps(path, wire) {
       wire.y += dy;
 
       step += 1;
-
     }
     posTaken.push({ ...wire });
 
@@ -42,29 +36,49 @@ function traceSteps(path, wire) {
   return posTaken;
 }
 
-function crosses(path1, path2) {
+function findMinimumSteps(path1, path2) {
   let minimumSteps = Infinity;
 
   for (let i = 1; i < path1.length; i++) {
 
     for (let j = 1; j < path2.length; j++) {
 
-      if (path1[i].x === path2[j].x &&
+      if (path1[i].x === path2[j].x && path1[i].y === path2[j].y) {
+        const currentSteps = path1[i].step + path2[j].step;
 
-        path1[i].y === path2[j].y) {
-
-        const current = path1[i].step + path2[j].step;
-
-        minimumSteps = Math.min(current, minimumSteps);
-
+        minimumSteps = Math.min(currentSteps, minimumSteps);
       }
     }
   }
   return minimumSteps;
 }
 
+function findMinimumDistance(path1, path2) {
+  let minimumDistance = Infinity;
+
+  for (let i = 1; i < path1.length; i++) {
+
+    for (let j = 1; j < path2.length; j++) {
+
+      if (path1[i].x === path2[j].x && path1[i].y === path2[j].y) {
+        const currentDistance = Math.abs(path1[i].x) + Math.abs(path1[i].y);
+
+        minimumDistance = Math.min(currentDistance, minimumDistance);
+      }
+    }
+  }
+  return minimumDistance;
+}
+const steps1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'.split(',');
+const steps2 = 'U62,R66,U55,R34,D71,R55,D58,R83'.split(',');
+
+const wire1 = { x: 0, y: 0 };
+const wire2 = { x: 0, y: 0 };
+
 const path1 = traceSteps(steps1, wire1);
 const path2 = traceSteps(steps2, wire2);
 
-const minDistance = crosses(path1, path2);
-console.log({ minDistance });
+const minSteps = findMinimumSteps(path1, path2);
+const minimumDistance = findMinimumDistance(path1, path2);
+
+console.log({ minimumDistance, minSteps });
