28eebb55f4c0c9d0f8d49d0d506c9ede53156b2e
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-08 21:14:12 +0530

    `another one `

--- 2019/day_6/6_1.js
+++ 2019/day_6/6_1.js
@@ -1,45 +1,54 @@
-const pathMap = "COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L".split(',').map(pair => pair.split(')'));
-// 42
 const mapOrbiters = (pathMap) => {
   const map = {};
-  for (const [planet, orbiter] of pathMap) {
-    if (map[planet] === undefined) {
-      map[planet] = [];
-    }
 
-    map[planet].push(orbiter);
+  for (const [planet, orbiter] of pathMap) {
+
+    if (map[planet] === undefined) {
+      map[planet] = { orbiters: [] };
+    }
+    map[planet].orbiters.push(orbiter);
+
 
     if (map[orbiter] === undefined) {
-      map[orbiter] = [];
+      map[orbiter] = { orbiters: [] };
     }
+
   }
 
   return map;
 };
 
-const map = mapOrbiters(pathMap);
+const input = Deno.readTextFileSync('./2019/day_6/6.input').split('\n').map(pair => pair.split(')'));
+const map = mapOrbiters(input);
 
-const wrapper = (map) => {
-  let count = 0;
+// const pathMap = "COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L".split(',').map(pair => pair.split(')'));
+// const map = mapOrbiters(pathMap);
+// console.log(map);
 
-  const traverseOrbiters = (start ,map) => {
-    count += 1;
+let count = 0;
+// 247089
 
-    while (start !== undefined) {
-      start = traverseOrbiters(map[start], map);
+const countOrbiters = (start, map) => {
+
+  while (start !== undefined) {
+    const orbiters = map[start].orbiters;
+
+    if (orbiters.length !== 0) {
+
+      orbiters.forEach(orbiter => {
+        count += 1;
+
+        start = countOrbiters(orbiter, map);
+      });
     }
 
     return start;
   };
 
-  for (const planet in map) {
-
-    traverseOrbiters(planet, map);
-  }
-
-  return count;
 };
 
-const count = wrapper(map);
+for (const planet in map) {
+  countOrbiters(planet, map);
+}
 
-console.log({ count });
+console.log({ count }, 247089);
