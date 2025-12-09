b9590128327ddbbfb9f16dde74a56d86169f87f3
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-09 20:19:08 +0530

    `I am not documenting anything`

--- 2019/day_8/8_1.js
+++ 2019/day_8/8_1.js
@@ -1,7 +1,7 @@
 ///* 123456789012 */
 // 3 pixel x 2 pixel
 // ROW X COLUMN
-import { image } from "./input.js";
+// import { image } from "./input.js";
 // const ROW_COUNT = 25;
 // const COLUMN_COUNT = 6;
 
@@ -15,77 +15,75 @@ Layer 2: [[7,8,9]
 const image = '123456789012';
 const ROW_COUNT = 3;
 const COLUMN_COUNT = 2;
-
-
 const layersCount = image.length / (ROW_COUNT * COLUMN_COUNT);
 
 const showDisplay = (display) => {
   console.log(display.join('\n'));
 };
 
-let x = 0;
+function getLayer(image, x) {
+  let offset = x;
+  const layer = [];
 
-const plotPixels = () => {
-  
+  for (let cols = 0; cols < (ROW_COUNT * COLUMN_COUNT); cols++) {
+    layer.push(parseInt(image[offset++]));
+  }
+  return [layer, offset];
+}
+
+const plotPixels = (image) => {
+  let x = 0;
+  const display = [];
+
   for (let index = 0; index < layersCount; index++) {
-    const layer = [];
-
-    for (let cols = 0; cols < (ROW_COUNT * COLUMN_COUNT); cols++) {
-      layer.push(parseInt(image[x++]));
-    }
+    const [layer, offset] = getLayer(image, x);
+    x = offset;
 
     display.push(layer);
   }
+  return display;
 };
+const display = plotPixels(image);
 
-plotPixels();
-
-const countZeroesInLayer = (layer = []) => {
-
-  return layer.reduce((count, pixel) => pixel === 0 ? count + 1 : count, 0);
+const countZeroesInLayer = (layer) => {
+  return layer.reduce((count, pixel) => count + (pixel === 0 ? 1 : 0), 0);
 };
+const findLayerWithLeast0 = ((display) => {
+  return display.reduce((least0, layer, index) => {
+    const currentCount = countZeroesInLayer(layer);
 
-const leastZero = display.reduce((leastZero, layer, index) => {
+    if (least0.zeroCount > currentCount) {
+      least0.zeroCount = currentCount;
+      least0.index = index;
+    }
 
-  const currentZeroCount = countZeroesInLayer(layer);
-
-  if (leastZero.zeroCount > currentZeroCount) {
-
-    leastZero.zeroCount = currentZeroCount;
-    leastZero.index = index;
-  }
-
-  return leastZero;
-},
-  {
-    zeroCount: Infinity,
-    index: -1
-  });
+    return least0;
+  },
+    {
+      zeroCount: Infinity,
+      index: -1
+    });
 
 
+});
+const leastZero = findLayerWithLeast0(display);
 
 const { one, two } = display[leastZero.index].reduce((count2and1, current) => {
-  if (current === 2) count2and1.two += 1;
-  if (current === 1) count2and1.one += 1;
+  if (count2and1[current] !== undefined) {
+    count2and1[current] += 1;
+  }
 
   return count2and1;
 }, {
-  one: 0,
-  two: 0
+  1: 0,
+  2: 0
 });
-const display = [];
 
 console.log(display);
 
-showDisplay(display);
-
-
-console.log({ one, two });
-
-console.log(one * two);
-
+// console.log({ one, two });
+// console.log(one * two);
 /* 
 { one: 14, two: 128 }
 1792
-
 */
