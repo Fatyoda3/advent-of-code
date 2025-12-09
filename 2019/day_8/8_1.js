057c3591cad5fcfabd60e11ae900b52bbd7279be
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-09 20:31:03 +0530

    `changed something `

--- 2019/day_8/8_1.js
+++ 2019/day_8/8_1.js
@@ -1,9 +1,9 @@
 ///* 123456789012 */
 // 3 pixel x 2 pixel
 // ROW X COLUMN
-// import { image } from "./input.js";
-// const ROW_COUNT = 25;
-// const COLUMN_COUNT = 6;
+import { image } from "./input.js";
+const ROW_COUNT = 25;
+const COLUMN_COUNT = 6;
 
 /* 
 Layer 1: [[1,2,3]
@@ -12,13 +12,14 @@ Layer 2: [[7,8,9]
          [0,1,2]]
 */
 
-const image = '123456789012';
-const ROW_COUNT = 3;
-const COLUMN_COUNT = 2;
+// const image = '123456789012';
+// const ROW_COUNT = 3;
+// const COLUMN_COUNT = 2;
+
 const layersCount = image.length / (ROW_COUNT * COLUMN_COUNT);
 
 const showDisplay = (display) => {
-  console.log(display.join('\n'));
+  display.forEach((layer) => console.log(layer));
 };
 
 function getLayer(image, x) {
@@ -26,6 +27,7 @@ function getLayer(image, x) {
   const layer = [];
 
   for (let cols = 0; cols < (ROW_COUNT * COLUMN_COUNT); cols++) {
+
     layer.push(parseInt(image[offset++]));
   }
   return [layer, offset];
@@ -41,6 +43,7 @@ const plotPixels = (image) => {
 
     display.push(layer);
   }
+
   return display;
 };
 const display = plotPixels(image);
@@ -48,6 +51,7 @@ const display = plotPixels(image);
 const countZeroesInLayer = (layer) => {
   return layer.reduce((count, pixel) => count + (pixel === 0 ? 1 : 0), 0);
 };
+
 const findLayerWithLeast0 = ((display) => {
   return display.reduce((least0, layer, index) => {
     const currentCount = countZeroesInLayer(layer);
@@ -68,21 +72,21 @@ const findLayerWithLeast0 = ((display) => {
 });
 const leastZero = findLayerWithLeast0(display);
 
-const { one, two } = display[leastZero.index].reduce((count2and1, current) => {
-  if (count2and1[current] !== undefined) {
-    count2and1[current] += 1;
-  }
+const { '1': one, '2': two } = display[leastZero.index].reduce((count2and1, current) => {
+  count2and1[current] += 1;
+  console.log(count2and1);
 
   return count2and1;
 }, {
-  1: 0,
-  2: 0
+  '1': 0,
+  '2': 0,
+  '0': 0
 });
 
-console.log(display);
+// showDisplay(display);
 
-// console.log({ one, two });
-// console.log(one * two);
+console.log({ one, two });
+console.log(one * two);
 /* 
 { one: 14, two: 128 }
 1792
