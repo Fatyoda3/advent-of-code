f5fd4292089969b2c69d84e9f16782f4c052d18a
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-15 09:28:58 +0530

    `solved 2025 day 1 part 1 `

--- 2025/day_1/1_1.js
+++ 2025/day_1/1_1.js
@@ -8,7 +8,8 @@ L55
 L1
 L99
 R14
-L82`};
+L82`
+};
 
 let dial = 50;
 const maxRotation = 99 + 1;
@@ -27,6 +28,11 @@ const turn = (instruction, dial) => {
   if (newDialValue < 0) {
     newDialValue = 100 + newDialValue;
   }
+  if (newDialValue > 99) {
+    console.log({ newDialValue });
+    newDialValue = newDialValue - 100;
+
+  }
   return newDialValue;
 };
 
@@ -35,9 +41,13 @@ const parseIns = (instruction) => {
   return { direction, turnAmount: parseInt(remaining.join('')) };
 };
 
+let f = 0;
+
 input.first.split('\n').reduce((dial, currentTurnValue) => {
   const values = parseIns(currentTurnValue);
   dial = turn(values, dial);
+  console.log({ dial });
 
+  if (dial === 0) { f += 1; }
   return dial;
 }, 50);
