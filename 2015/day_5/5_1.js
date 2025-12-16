ae9892284954dba05cb09efd191cdcae6ca44da1
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-16 14:17:13 +0530

    ``

--- 2015/day_5/5_1.js
+++ 2015/day_5/5_1.js
@@ -4,8 +4,13 @@ const VOWELS = [
   "u", "U"];
 
 
-const hasAtLeast3Vowel = (text) => {
+const hasAtLeast3Vowel = (letters) => {
   let count = 0;
-  
+
+  for (const letter of letters) {
+    if (VOWELS.includes(letter)) {
+      count += 1;
+    }
+  }
   return count >= 3;
 };
