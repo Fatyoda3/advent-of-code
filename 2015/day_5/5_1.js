80f6a25c0b77d75dcbd48121f33b356b99e78a5d
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-16 14:45:46 +0530

    ``

--- 2015/day_5/5_1.js
+++ 2015/day_5/5_1.js
@@ -1,8 +1,19 @@
+import { puzzle } from "./input.js";
 const VOWELS = [
   "a", "A", "e", "E",
   "i", "I", "o", "O",
   "u", "U"];
 
+const forbiddenWords = ['ab', 'cd', 'pq', 'xy'];
+
+const hasConsecutiveSame = (letters) => {
+  for (let index = 1; index < letters.length; index++) {
+    if (letters[index - 1] === letters[index]) {
+      return true;
+    }
+  }
+  return false;
+};
 
 const hasAtLeast3Vowel = (letters) => {
   let count = 0;
@@ -14,3 +25,23 @@ const hasAtLeast3Vowel = (letters) => {
   }
   return count >= 3;
 };
+
+const hasForbiddenWords = (letters) => {
+  for (const forbiddenWord of forbiddenWords) {
+    if (letters.includes(forbiddenWord)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const isNiceString = (string) => {
+  return hasConsecutiveSame(string)
+    && hasAtLeast3Vowel(string)
+    && !(hasForbiddenWords(string));
+};
+
+const countOfNice = puzzle.reduce((count, naughtyString) => {
+  return isNiceString(naughtyString) ? count + 1 : count;
+}, 0);
+console.log({ countOfNice });
