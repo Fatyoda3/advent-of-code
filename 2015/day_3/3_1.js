914148ccd9757b45f91e3eab77b2c79fb162621a
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-16 12:14:49 +0530

    ``

--- 2015/day_3/3_1.js
+++ 2015/day_3/3_1.js
@@ -1,12 +1,27 @@
+import { puzzle } from "./input.js";
 const NORTH = '^';
 const SOUTH = 'v';
 const EAST = '>';
 const WEST = '<';
-
-
-const deliverPresents = (dirs) => {
-
-
-  console.log({ presentCount });
-
+const presentsInDirection = {
+  [NORTH]: 0,
+  [SOUTH]: 0,
+  [EAST]: 0,
+  [WEST]: 0
 };
+
+const deliverPresents = (directions) => {
+  const presentsInDirectionCopy = { ...presentsInDirection };
+  for (const direction of directions) {
+
+    presentsInDirectionCopy[direction] += 1;
+  }
+
+  console.log({ presentsInDirectionCopy });
+  return presentsInDirectionCopy;
+};
+
+(deliverPresents('>'));
+(deliverPresents('^>v<'));
+(deliverPresents('^v^v^v^v^v'));
+(deliverPresents(puzzle));
---
+++ 2015/day_5/5_1.js
@@ -0,0 +1,11 @@
+const VOWELS = [
+  "a", "A", "e", "E",
+  "i", "I", "o", "O",
+  "u", "U"];
+
+
+const hasAtLeast3Vowel = (text) => {
+  let count = 0;
+  
+  return count >= 3;
+};
