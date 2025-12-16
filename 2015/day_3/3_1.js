3a29bf7b19c4708ff316a02466c8bc3b750d5598
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-16 20:49:21 +0530

    `just solving a mix of problems `

--- 2015/day_3/3_1.js
+++ 2015/day_3/3_1.js
@@ -1,27 +1,35 @@
 import { puzzle } from "./input.js";
-const NORTH = '^';
-const SOUTH = 'v';
-const EAST = '>';
-const WEST = '<';
-const presentsInDirection = {
-  [NORTH]: 0,
-  [SOUTH]: 0,
-  [EAST]: 0,
-  [WEST]: 0
+const [NORTH, SOUTH, EAST, WEST] = ['^', 'v', '>', '<'];
+const heading = {
+  [NORTH]: [0, 1],
+  [SOUTH]: [0, -1],
+  [EAST]: [1, 0],
+  [WEST]: [-1, 0],
 };
 
 const deliverPresents = (directions) => {
-  const presentsInDirectionCopy = { ...presentsInDirection };
-  for (const direction of directions) {
+  const visited = [];
+  const current = { x: 0, y: 0 };
+  let count = 0;
 
-    presentsInDirectionCopy[direction] += 1;
+  for (const direction of directions) {
+    const [dx, dy] = heading[direction];
+
+    if (!(visited.find(prev => current.x === prev.x && current.y === prev.y))) {
+      visited.push({ ...current });
+      count += 1;
+    }
+
+    current.x += dx;
+    current.y += dy;
   }
 
-  console.log({ presentsInDirectionCopy });
-  return presentsInDirectionCopy;
+  return { count, visited };
 };
 
-(deliverPresents('>'));
-(deliverPresents('^>v<'));
-(deliverPresents('^v^v^v^v^v'));
-(deliverPresents(puzzle));
+// const { count , visited } = deliverPresents('>');
+// const { count, visited } = deliverPresents('^>v<');
+// const { count , visited } = deliverPresents('^v^v^v^v^v');
+// const { count, visited } = deliverP   resents(puzzle);
+// console.log({ count, visited });
+
