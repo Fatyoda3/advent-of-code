8f0997839f96b93fcae507cb2848220c83d21164
fatyoda3 <shivangsingh654@gmail.com>
2025-12-23 12:05:18 +0530

    made some validations for the problem

--- 2020/day_4/4_1.js
+++ 2020/day_4/4_1.js
@@ -1,22 +1,20 @@
 import { passports } from "./passports.js";
 // const puzzle = Deno.readTextFileSync("./2020/day_4/puzzle.txt");
 
-const parsePassports = (passports) => {
-  return passports
-    .split("\n\n")
-    .map((fields) => fields.split("\n").join(" "));
-};
-
-const genOnePassport = (fields) => {
-  return fields.split(" ").reduce((passport, field) => {
+const formatePassportDetail = (fields) => {
+  return fields.reduce((passport, field) => {
     const [key, value] = field.split(":");
     passport[key] = value;
     return passport;
   }, {});
 };
 
-const generatePassports = (passports) => {
-  return parsePassports(passports).map(genOnePassport);
+const parsePassports = (passports) => {
+  const passportsData = passports
+    .split("\n\n")
+    .map((fields) => fields.split("\n").join(" ").split(" "));
+
+  return passportsData.map(formatePassportDetail);
 };
 
 const fieldsToValidate = [
@@ -27,7 +25,6 @@ const fieldsToValidate = [
   "hcl",
   "ecl",
   "pid",
-  // "cid",
 ];
 
 const allFieldsPresent = (passport) => {
@@ -35,5 +32,60 @@ const allFieldsPresent = (passport) => {
     .every((field) => field in passport);
 };
 
-const [one, two, three, four] = generatePassports(passports);
-console.log({ one }, allFieldsPresent(one));
+const parseHeight = (height = "") => {
+  const x = parseInt(height.slice(0, -2));
+  console.log({ x });
+
+  if (height.slice(-2) === "cm") {
+    console.log(height.slice(-2), "cm");
+    return x >= 150 && x <= 193;
+  }
+  if (height.slice(-2) === "in") {
+    console.log(height.slice(-2), "in");
+    return x >= 59 && x <= 76;
+  }
+  return false;
+};
+const isValidHair = (value = "") => {
+  const hex = /^#[\da-f]{6}$/;
+  return hex.test(value);
+};
+const EYE_COLORS = [
+  "amb",
+  "blu",
+  "brn",
+  "gry",
+  "grn",
+  "hzl",
+  "oth",
+];
+const isValidEyeColor = (value) => {
+  return EYE_COLORS.includes(value);
+};
+
+const isValidPID = (value) => {
+  const pid = /^\d{9}$/;
+  return pid.test(value);
+};
+
+const validateFields = (passport) => {
+  const isValidBYR = passport.byr >= 1920 && passport.byr <= 2002;
+  const isValidIYR = passport.iyr >= 2010 && passport.iyr <= 2020;
+  const isValidEYR = passport.eyr >= 2020 && passport.eyr <= 2030;
+
+  const isValidHGT = parseHeight(passport.HGT);
+
+  const isValidHCL = isValidHair(passport.hcl);
+  const ValidPID = isValidPID(passport.pid);
+  const isValidECL = isValidEyeColor(passport.ecl);
+
+  return isValidBYR && isValidIYR && isValidHGT &&
+    isValidEYR && isValidHCL && ValidPID && isValidECL;
+};
+
+const [one, two, three, four] = parsePassports(passports);
+console.log(
+  { one /* , two, three, four */ },
+  allFieldsPresent(one),
+  validateFields(one),
+);
