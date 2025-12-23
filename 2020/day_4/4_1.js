2330e3355f483037115457d004c0045e7a5bdab5
fatyoda3 <shivangsingh654@gmail.com>
2025-12-23 10:09:09 +0530

    Solving day 4 passport

--- 2020/day_4/4_1.js
+++ 2020/day_4/4_1.js
@@ -1,25 +1,10 @@
-const passports = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 iyr:2017 cid:147 hgt:183cm
-
-iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-hcl:#cfa07d byr:1929
-
-hcl:#ae17e1 iyr:2013
-eyr:2024
-ecl:brn pid:760753108 byr:1931
-hgt:179cm
-
-hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in`;
+import { passports } from "./passports.js";
 
 const parsePassports = (passports) => {
   return passports
     .split("\n\n")
     .map((fields) => fields.split("\n").join(" "));
 };
-
-const test =
-  "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm";
 
 const generatePassport = (fields) => {
   return fields.split(" ").reduce((passport, field) => {
@@ -28,3 +13,11 @@ const generatePassport = (fields) => {
     return passport;
   }, {});
 };
+
+const generatePassports = (passports) => {
+  return parsePassports(passports).map(generatePassport);
+};
+
+const passportsFormatted = generatePassports(passports);
+
+console.log(passportsFormatted);
