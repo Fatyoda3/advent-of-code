2ca972fc301ce499c8f8b02b83adf37588566cb2
fatyoda3 <shivangsingh654@gmail.com>
2025-12-24 19:48:29 +0530

    solved day_3 part 2

---
+++ 2020/day_4/4_2.js
@@ -0,0 +1,88 @@
+import { passports } from "./passports.js";
+const puzzle = Deno.readTextFileSync("./2020/day_4/puzzle.txt");
+
+const formatePassportDetail = (fields) => {
+  return fields.reduce((passport, field) => {
+    const [key, value] = field.split(":");
+    passport[key] = value;
+    return passport;
+  }, {});
+};
+const parsePassports = (passports) => {
+  const passportsData = passports
+    .split("\n\n")
+    .map((fields) => fields.split("\n").join(" ").split(" "));
+
+  return passportsData.map(formatePassportDetail);
+};
+const fieldsToValidate = [
+  "byr",
+  "iyr",
+  "eyr",
+  "hgt",
+  "hcl",
+  "ecl",
+  "pid",
+];
+const allFieldsPresent = (passport) => {
+  return fieldsToValidate
+    .every((field) => field in passport);
+};
+
+const validateHeight = (height = "") => {
+  console.log({ height });
+
+  const x = parseInt(height.slice(0, -2));
+  if (height.slice(-2) === "cm") {
+    return x >= 150 && x <= 193;
+  }
+  if (height.slice(-2) === "in") {
+    return x >= 59 && x <= 76;
+  }
+  return false;
+};
+
+const isValidHair = (value) => {
+  const hex = /^#[\da-f]{6}$/;
+  return hex.test(value);
+};
+const EYE_COLORS = [
+  "amb",
+  "blu",
+  "brn",
+  "gry",
+  "grn",
+  "hzl",
+  "oth",
+];
+const isValidEyeColor = (value) => {
+  return EYE_COLORS.includes(value);
+};
+
+const isValidPID = (value) => {
+  const pid = /^\d{9}$/;
+  return pid.test(value);
+};
+
+const validateFields = (passport) => {
+  const isValidBYR = passport.byr >= 1920 && passport.byr <= 2002;
+  const isValidIYR = passport.iyr >= 2010 && passport.iyr <= 2020;
+  const isValidEYR = passport.eyr >= 2020 && passport.eyr <= 2030;
+
+  const isValidHGT = validateHeight(passport.hgt);
+
+  const isValidHCL = isValidHair(passport.hcl);
+  const ValidPID = isValidPID(passport.pid);
+  const isValidECL = isValidEyeColor(passport.ecl);
+
+  return isValidBYR && isValidIYR && isValidHGT &&
+    isValidEYR && isValidHCL && ValidPID && isValidECL;
+};
+
+const count = parsePassports(puzzle).reduce((count, current) => {
+  return allFieldsPresent(current) && validateFields(current)
+    ? count + 1
+    : count;
+}, 0);
+
+console.log({ count });
--- 2025/day_3/3_2.js
+++ 2025/day_3/3_2.js
@@ -1,28 +1,74 @@
-const example =
-  `987654321111111
-811111111111119
-234234234234278
-818181911112111`
-    .split('\n');
+// const batteryPack = "234234234234278";
+// const batteryPack = "811111111111119";
+// const batteryPacks = [
+//   "987654321111111",
+//   "811111111111119",
+//   "234234234234278",
+//   "818181911112111",
+// ];
+const batteryPack = Deno.readTextFileSync("./day_3/input.txt").split("\n");
 
-const batteryPack = ['987654321111111'];
-//9 scan ahead if it is highest true then push
-// if some larger is found then use it and then reset 
-// const batteryPack = Deno.readTextFileSync('./2025/day_3/input.txt').split('\n');
-// 12 digits
-// skip some digits in between
-// maintain order 
-const t = '811111111111119';
+//15 -12 at most 3 digits --4
+//last value IDX
+//15 -12-1 at most 2 digits --3
+//last value IDX
+//15 -12-2 at most 1 digits --4
+//last value IDX
 
-const getMaxValue = (battery) => {
-  const formed = [];
+//15 -12-3 at most 1 digits --4
 
-// for (let i = 0; i < array.length; i++) {
-//   const element = array[i];
-  
-// }
+// const maxJolt = (battery) => {
+//   const temps = [];
 
+//   let remaining = 12;
+//   let prevIdx = 0;
+
+//   while (remaining >= 0) {
+//     let max = -Infinity;
+
+//     const maxChecks = battery.length - 12 - temps.length - 1;
+
+//     if (maxChecks === 0) {
+//       temps.push(...battery.slice(prevIdx + 1, remaining));
+//       console.log(temps.join(""));
+
+//       return;
+//     }
+
+//     console.log({ maxChecks, prevIdx });
+//     for (let i = prevIdx; i <= maxChecks; i++) {
+//       if (max < parseInt(battery[i])) {
+//         max = parseInt(battery[i]);
+//       }
+//     }
+
+//     prevIdx = battery.indexOf(max + "", prevIdx) + 1;
+//     temps.push(max);
+//     // prevIdx += 1;
+//     remaining -= 1;
+//   }
+
+//   console.log("-------------------------------------");
+//   console.log(temps.join(""));
+// };
+
+const maxJolt = (battery) => {
+  const highest = [];
+  const required = 12;
+  let idx = 0;
+
+  while (highest.length !== required) {
+    let max = -Infinity;
+    const maxChecks = battery.length - (required - highest.length - 1);
+    for (let i = idx; i < maxChecks; i++) {
+      max = Math.max(max, +battery[i]);
+    }
+    idx = battery.indexOf(max + "", idx) + 1;
+
+    highest.push(max);
+  }
+  return +(highest.join(""));
 };
 
-const puzzleAnswer = example.reduce((prev, battery) => prev + getMaxValue(battery), 0);
-console.log({ puzzleAnswer });
+const x = batteryPack.reduce((sum, bP) => sum + maxJolt(bP), 0);
+console.log({ x });
